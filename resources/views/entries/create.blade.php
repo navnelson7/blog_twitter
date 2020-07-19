b2047a466040eaee9a52ec5d88c58a08b0a4867b
Juan Navarro <navnelson7@gmail.com>
2020-07-19 00:17:14 -0600

    Agregando la vista Entries con su formulario y validacion del mismo

--- resources/views/entries/create.blade.php
+++ resources/views/entries/create.blade.php
@@ -1,1 +1,62 @@
-<h1>Hola desde la viste create entries</h1>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('New Entry') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    {{--  --}}
+                    <form action="" method="POST">
+                        @csrf
+                        {{-- Agregando el title --}}
+                        <div class="form-group row">
+                            <label for="title" class="col-md-4 col-form-label text-md-right">Title</label>
+
+                            <div class="col-md-6">
+                                <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="title">
+
+                                @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div> 
+                        </div>
+                        {{-- Finalizando el title --}}
+                        {{-- Agregando el content --}}
+                        <div class="form-group row">
+                            <label for="title" class="col-md-4 col-form-label text-md-right">Content</label>
+
+                            <div class="col-md-6">
+                                <textarea id="content" class="form-control @error('content') is-invalid @enderror" name="content"  required>{{old('content')}}</textarea>
+
+                                @error('content')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        {{-- Finalizando el contente --}}
+                        {{-- Agregando el button --}}
+                        <div class="form-group ">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary btn-lg btn-block">Publish</button>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
