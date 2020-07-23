84727319f2e019526cd5c8400a10d94f9c8b53bc
Juan Navarro <navnelson7@gmail.com>
2020-07-22 22:48:33 -0600

    Mejorando la vista de las entradas colocandolas en card y su respectivo id

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,16 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Last Entries</div>
-
-                <div class="card-body">
-                    @foreach ($entries as $entry)
-                        <p>{{ $entry->title}}</p>
+            @foreach ($entries as $entry)
+                <div class="card mt-2">
+                    <div class="card-header">
+                        {{ $entry->id}}. {{ $entry->title}}
+                    </div>
+                    <div class="card-body">
                         <p>{{ $entry->content}}</p>
-                    @endforeach
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
