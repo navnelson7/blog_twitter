ff4d593307869f14dbf200f13004204b5776d4c8
Juan Navarro <navnelson7@gmail.com>
2020-08-03 20:30:34 -0600

    Mostrando entradas por cada usuario en su vista de entradas individual

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Entry;
 class UserController extends Controller
 {
     //creando la vista del usuario
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $entries = Entry::where('user_id', $user->id)->get();
+        return view('users.show', compact('user','entries'));
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,19 +3,36 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-4">
             <div class="card">
-                <div class="card-header">{{ $user->name }}</div>
-
+                <div class="card-header">Tweets</div>
+    
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    
+                    Twitter api soon
                 </div>
             </div>
         </div>
-    </div>
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ $user->name }}</div>
+    
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <p>Published Entries</p>
+                        <ul>
+                            @foreach ($entries as $entry)
+                                <li>
+                                    <a href="{{url('entries/'.$entry->id)}}">{{ $entry->title }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    </div>    
+</div>
 @endsection
